5de8f6858fc6e99d34ffcb410e0e118957dfafbf
razorbanana <stanislav.stefyuk2@gmail.com>
2023-05-28 17:27:52 +0300

    feat: 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,3 +1,5 @@
+import Course from "./components/Course"
+
 const App = () => {
   const courses = [
     {
@@ -51,51 +53,6 @@ const App = () => {
         <Course course={course} key={course.id}/>
       )}
     </div>
-  )
-}
-
-const Course = ({course}) => {
-  
-  return (
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts}/> 
-      <Total parts={course.parts}/>
-    </div>
-  )
-}
-
-const Content = ({parts}) => {
-  console.log(parts)
-  return (
-    <div>
-      {parts.map(part => 
-        <Part part={part} key={part.id}/>
-      )}
-    </div>
-  )
-}
-
-const Header =({name})=>{
-  console.log(name)
-  return (
-    <h2>{name}</h2>
-  )
-}
-
-const Part =({part})=>{
-  console.log(part)
-  return (
-    <p>
-        {part.name} {part.exercises}
-      </p>
-  )
-}
-
-const Total =({parts})=>{
-  console.log(parts)
-  return (
-    <p><b>total of {parts.reduce((acc, part)=>acc+part.exercises,0)} exercises</b></p>
   )
 }
 
