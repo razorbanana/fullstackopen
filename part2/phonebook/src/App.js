62e9fe71bd45c65655d4ea6e8f05c2bd50861979
razorbanana <stanislav.stefyuk2@gmail.com>
2023-05-29 10:43:50 +0300

    feat: 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,17 +1,19 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456'},
-    { name: 'Ada Lovelace', number: '39-44-5323523'},
-    { name: 'Dan Abramov', number: '12-43-234345'},
-    { name: 'Mary Poppendieck', number: '39-23-6423122'}
-  ])
+const App = ({data}) => {
+  const [persons, setPersons] = useState(data)
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
 
   console.log(newName)
+
+  useEffect(()=>{
+    axios.get("http://localhost:3001/persons").then(response=>{
+      setPersons(response.data)
+    })
+  },[])
 
   const handleFilterChange = (event) => {
     setFilter(event.target.value)
