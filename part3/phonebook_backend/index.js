b1a2c8ea3b01132f799fafc095d5561b8edef79d
razorbanana <stanislav.stefyuk2@gmail.com>
2023-05-31 12:57:03 +0300

    feat: 3.4

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 
-const persons = [
+let persons = [
     { 
       "id": 1,
       "name": "Arto Hellas", 
@@ -33,15 +33,27 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find(person => person.id == id)
-    response.json(person)
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if(person){
+        response.json(person)
+    }else{
+        response.status(404).end()
+    }
+    
 })
 
 app.get('/info', (request, response) => {
     let date = new Date(Date.now()).toUTCString()
     response.send(`<p>Phonebook has info about ${persons.length} people</p><p>${date}</p>`)
 })
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id != id)
+    response.status(204).end()
+})
+
 
 const PORT = 3001
 app.listen(PORT, () => {
