96349a7f35391c578c034b55a7b94f84cb699214
razorbanana <stanislav.stefyuk2@gmail.com>
2023-06-13 12:10:12 +0300

    feat: 4.4

--- part4/blog/jest_tests/test.js
+++ part4/blog/jest_tests/test.js
@@ -1,8 +1,37 @@
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-  const blogs = []
+describe('dummy', () => {
+  test('dummy returns one', () => {
+    const blogs = []
+  
+    const result = listHelper.dummy(blogs)
+    expect(result).toBe(1)
+  })
+})
 
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
+describe('Total likes', () => {
+  test('total likes of []', () => {
+    const blogs = []
+  
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(0)
+  })
+
+  test('total likes of blogs with 1 and 2 likes', () => {
+    const blogs = [{
+      "title": "Example 1",
+      "author": "me",
+      "url": "/someurl1",
+      "likes": 1
+    },
+    {
+      "title": "Example 2",
+      "author": "me",
+      "url": "/someurl2",
+      "likes": 2
+    }]
+  
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(3)
+  })
 })
